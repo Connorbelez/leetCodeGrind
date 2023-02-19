835c63352467d17bf007b95ee2bdbd38a15eba46
Connorbelez <connor.belez@gmail.com>
2023-02-19 14:14:03 -0500

    groupAnagrams

---
+++ arrays_hashing/groupAnagrams.py
@@ -0,0 +1,29 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anaMap = {}
+        anaList = []
+        for i,val in enumerate(strs):
+            sortedWord = list(val)
+            sortedWord.sort()
+            sortedWord = "".join(sortedWord)
+
+
+            if sortedWord not in anaMap:
+                anaMap[sortedWord] = [i]
+            else:
+                anaMap[sortedWord].append(i)
+        
+        for key,valList in anaMap.items():
+            row = [strs[i] for i in valList]
+            anaList.append(row)
+
+        return anaList
+    
+def main():
+    s = Solution()
+    print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+    print(s.groupAnagrams([""]))
+    print(s.groupAnagrams(["a"]))
+
+if __name__ == "__main__":
+    main()
