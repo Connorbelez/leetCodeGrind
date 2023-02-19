753952ae27e8e7594cb79c5c32391488a3a39fae
Connorbelez <connor.belez@gmail.com>
2023-02-19 14:12:24 -0500

    twoSum

---
+++ arrays_hashing/twoSum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        for i,val in enumerate(nums):
+            remainder = target - val
+            if remainder in numMap:
+                indexOfRemainder = numMap[remainder]
+                return [indexOfRemainder, i]
+
+            if val not in numMap:
+                numMap[val] = i
+                
+
+def main():
+    s = Solution()
+    print(s.twoSum([2,7,11,15], 9))
+    print(s.twoSum([3,2,4], 6))
+    print(s.twoSum([3,3], 6))
+
+if __name__ == "__main__":
+    main()
